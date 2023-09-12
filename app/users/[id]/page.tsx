6dfb957aeb4b4fbfc745fb46f4203c3850926063
Florent <florent.vincerot@me.com>
2023-09-12 14:01:14 +0200

    dynamic route

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,10 @@
+// app/users/[id]/page.tsx
+
+interface Props {
+  params: { id: number };
+}
+const UserDetailPage = ({ params: { id } }: Props) => {
+  return <div>UserDetailPage {id}</div>;
+};
+
+export default UserDetailPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,12 @@
 // app / users / page.tsx;
 
-interface User {
-  name: string;
-  id: number;
-  email: string;
-}
+import UserTable from "./UserTable";
 
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
+const UsersPage = () => {
   return (
     <>
       <div>UsersPage</div>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th> Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
