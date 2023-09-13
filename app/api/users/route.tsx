20ba062c93dc7a36a207268290ba9f7d9bb8c914
Florent <florent.vincerot@me.com>
2023-09-13 17:18:45 +0200

    updating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -14,11 +14,33 @@ export async function POST(request: NextRequest) {
 
   // Validate
   // If invalid, return 400
-  // Else Return
+  // Else Return the body
 
   if (!body.name)
     return NextResponse.json({ error: "name is required" }, { status: 404 });
 
   // return the body
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // validate the request body
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json(
+      // If invalid, return 400 error (bad request)
+      { error: "invalid request, namer is required" },
+      { status: 400 }
+    );
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
+
+  // Fetch the user with the given id
+  // If doesn't exist, return 404
+  // Update the user
+  // Return the updated user
 }
