92b7e440706b0892464f825890b0789f1fb192d6
Florent <florent.vincerot@me.com>
2023-09-14 16:04:49 +0200

    configure and init database with Prisma, define schema

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -11,9 +11,6 @@ export function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  // fetch data from a db
-  // If not found,return 404 error
-  // Else return datas
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "flo" });
@@ -23,32 +20,18 @@ export async function PUT(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  // validate the request body
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json(
-      // If invalid, return 400 error (bad request)
-      { error: "invalid request, namer is required" },
-      { status: 400 }
-    );
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
-
-  // Fetch the user with the given id
-  // If doesn't exist, return 404
-  // Update the user
-  // Return the updated user
 }
-
 export function DELETE(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  // fetch user from database
-  // If not found return 404
-  // otherwise, delete the user from database
-  // return 200 response
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({});
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,23 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
 export function GET(request: NextRequest) {
-  // in a real world, fetch database here
   return NextResponse.json([
     { id: 1, name: "John" },
     { id: 2, name: "Paul" },
   ]);
 }
 export async function POST(request: NextRequest) {
-  // read the body of the request
   const body = await request.json();
+  const validation = schema.safeParse(body);
 
-  // Validate
-  // If invalid, return 400
-  // Else Return the body
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 404 });
 
-  if (!body.name)
-    return NextResponse.json({ error: "name is required" }, { status: 404 });
-
-  // return the body
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
