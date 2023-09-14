48a9ab4b2bd62e3e2c8ad1740b62ee0cf4b9c1b6
Florent <florent.vincerot@me.com>
2023-09-14 09:02:05 +0200

    update & delete debugging

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,6 @@
 // app/api/users/route.tsx
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 export function GET(request: NextRequest) {
   // in a real world, fetch database here
@@ -21,39 +22,4 @@ export async function POST(request: NextRequest) {
 
   // return the body
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
-}
-
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { id: number } }
-) {
-  // validate the request body
-  const body = await request.json();
-  if (!body.name)
-    return NextResponse.json(
-      // If invalid, return 400 error (bad request)
-      { error: "invalid request, namer is required" },
-      { status: 400 }
-    );
-  if (params.id > 10)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: body.name });
-
-  // Fetch the user with the given id
-  // If doesn't exist, return 404
-  // Update the user
-  // Return the updated user
-}
-
-export function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: number } }
-) {
-  // fetch user from database
-  // If not found return 404
-  // otherwise, delete the user from database
-  // return 200 response
-  if (params.id > 10)
-    return NextResponse.json({ error: "Usernot found" }, { status: 404 });
-  return NextResponse.json({});
 }
