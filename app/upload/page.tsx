b6ad0af9e7f27c183e101e96ccd25e8aa5a58c88
Florent <florent.vincerot@me.com>
2023-09-21 21:52:05 +0200

    display uploaded image on the widget page, customize styles

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,18 +1,51 @@
 // app/upload/page.tsx
 "use client";
 
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="dnajczzs">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="portrait image"
+        ></CldImage>
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="dnajczzs"
+        options={{
+          sources: ["local", "url", "camera"],
+          multiple: false,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#0f192c",
+              sourceBg: "#1c293a",
+            },
+          },
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
