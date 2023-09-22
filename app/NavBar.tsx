1b7fac01724e32b338a3ec76b0a05f1fc7dd5cb7
Florent <florent.vincerot@me.com>
2023-09-22 11:42:18 +0200

    nav bar links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,30 @@
 import Link from "next/link";
 import React from "react";
+import { number } from "zod";
 
 const NavBar = () => {
+  const routes = [
+    { label: "Users", path: "/users" },
+    { label: "Users api", path: "/api/users" },
+    { label: "Products", path: "/products" },
+  ];
   return (
-    <div className="flex bg-slate-200 dark:bg-gray-800 p-5 text-slate-900 dark:text-slate-400">
-      <Link href="/" className="mr-5">
-        Mon logo
-      </Link>
-      <Link href="/users">Users</Link>
-    </div>
+    <>
+      <div className="flex justify-between bg-slate-200 dark:bg-gray-800 p-5 text-slate-900 dark:text-slate-400">
+        <div>
+          <Link href="/" className="mr-5">
+            Mon logo
+          </Link>
+        </div>
+        <div>
+          {routes.map((r, index) => (
+            <Link key={index} href={r.path} className="pl-5">
+              {index + " " + r.label}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
