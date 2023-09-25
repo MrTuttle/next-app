8874b3f0b6e92ea4a18e448c44b392a1d52db90a
Florent <florent.vincerot@me.com>
2023-09-25 22:22:15 +0200

    accessing auth seesion on the client, loader & login in the navabar, use react context component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { number } from "zod";
+import { useSession } from "next-auth/react";
+import loading from "./users/loading";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   const routes = [
     { label: "Users", path: "/users" },
     { label: "Users api", path: "/api/users" },
     { label: "Products", path: "/products" },
-    { label: "Login", path: "/api/auth/signin" },
   ];
   return (
     <>
@@ -23,6 +28,11 @@ const NavBar = () => {
               {index + " " + r.label}
             </Link>
           ))}
+          {status === "loading" && <div>... Loading</div>}
+          {status === "authenticated" && <div>{session.user!.name}</div>}
+          {status === "unauthenticated" && (
+            <Link href="api/auth/signin">Login</Link>
+          )}
         </div>
       </div>
     </>
---
+++ app/api/auth/token/route.ts
@@ -0,0 +1,8 @@
+// app/api/auth/token/Route.ts
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const token = await getToken({ req: request });
+  return NextResponse.json(token);
+}
