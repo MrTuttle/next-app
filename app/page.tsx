532851f54ae230857c86c852bd41c1558c247190
Florent <florent.vincerot@me.com>
2023-09-26 14:06:59 +0200

    accessing server auth session

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,14 @@ import ProductCard from "./components/ProductCard";
 import FloComponent from "./components/FloComponent";
 import UsersList from "./components/UsersList";
 import GetUser from "./components/GetUser";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <FloComponent></FloComponent>
       <UsersList></UsersList>
       <Link href="/users">Users</Link>
