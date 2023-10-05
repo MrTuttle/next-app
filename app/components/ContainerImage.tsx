f1dce1d3126002c40eb94573dfe45e039b01cb5d
Florent <florent.vincerot@me.com>
2023-10-06 01:38:05 +0200

    ContainerImage transition on viewport

--- app/components/ContainerImage.tsx
+++ app/components/ContainerImage.tsx
@@ -1,23 +1,21 @@
 // import React from "react";
 import Image, { ImageProps, StaticImageData } from "next/image";
 import "./ContainerImage.css";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useInView } from "framer-motion";
 // to touch css class properties
-// import { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 interface Props {
   src: string | StaticImageData;
   alt: string;
 }
 
 const ContainerImage = ({ src, alt }: Props) => {
-  // const ref = useRef(null);
+  const ref = useRef(null);
+  const isInView = useInView(ref);
 
-  // useEffect(() => {
-  //   const element2 = ref.current;
-  //   console.log(`log : ${element2}`);
-  //   const element = document.querySelector(".imgBloc");
-  //   console.log(`log Element : ${element}`);
-  // }, []);
+  useEffect(() => {
+    console.log(`Element is in view: ${ref.current}`, isInView);
+  }, [isInView]);
 
   const x = 8.7;
   const y = 8.7;
@@ -27,15 +25,39 @@ const ContainerImage = ({ src, alt }: Props) => {
 
   return (
     <>
-      <div className="imgBloc" style={{ width: `${w}vw`, height: `${h}vh` }}>
+      <div
+        className="imgBloc"
+        // style={{ width: `${w}vw`, height: `${h}vh` }}
+        style={{
+          width: isInView ? 400 : 0,
+          height: `${h}`,
+          transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
+        }}
+        // style={{
+        //   width: `${w}vw`,
+        //   height: `${h}vh`,
+
+        //   opacity: isInView ? 1 : 0,
+        //   transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        // }}
+      >
         <Image
+          ref={ref}
           src={src}
           alt={alt}
           width="300"
           height="100"
           style={{
-            transform: `matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ${y}, ${x}, 0, ${zoom})`,
+            transform: isInView
+              ? "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 10, 0, 0, 0.9)"
+              : "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0.9)",
+            // opacity: isInView ? 1 : 0,
+            transition: "all 3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
           }}
+          // style={{
+          //   transform: `matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ${y}, ${x}, 0, ${zoom})`,
+          // }}
+          /////////////
           // className="object-cover"
           // sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
           // priority
--- app/loco/page.tsx
+++ app/loco/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import portrait from "@/public/images/16.jpg";
+import portrait16 from "@/public/images/16.jpg";
+import portrait1 from "@/public/images/1.jpg";
+import portrait3 from "@/public/images/3.jpg";
+import portrait13 from "@/public/images/13.jpg";
 import "../components/ContainerImage.css";
 import ContainerImage from "../components/ContainerImage";
 // to touch css class properties
@@ -9,50 +12,121 @@ import ContainerImage from "../components/ContainerImage";
 const loco = () => {
   return (
     <>
-      <div className="w-3/4 py-10 text-slate-500">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus facere labore
-          aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
-          eum rem nemo!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus facere labore
-          aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
-          eum rem nemo!
-        </p>
-      </div>
-      <ContainerImage src={portrait} alt="portrait" />
-      <div>
-        <h1>loco</h1>
-      </div>
-      <div className="w-3/4 py-10 text-slate-500">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus facere labore
-          aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
-          eum rem nemo!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus facere labore
-          aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
-          eum rem nemo!
-        </p>
-      </div>
+      <section>
+        <div className="w-3/4 py-10 text-slate-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+        </div>
+        <ContainerImage src={portrait16} alt="portrait" />
+        <div>
+          <h1>loco</h1>
+        </div>
+        <div className="w-3/4 py-10 text-slate-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="w-3/4 py-10 text-slate-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+        </div>
+      </section>
+      <section>
+        <ContainerImage src={portrait1} alt="portrait" />
 
-      <div className="imgBloc">
-        <Image
-          src={portrait}
-          alt="maison moderne bardage"
-          width="300"
-          height="100"
-          // className="object-cover"
-          // sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-          // priority
-        />
-      </div>
+        <div className="w-3/4 py-10 text-slate-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo! Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Praesentium deleniti dolore nemo
+            aliquid quam! Amet, fugiat? Ducimus facere labore aperiam adipisci,
+            illo voluptatibus. Quae numquam nostrum consequuntur eum rem nemo!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="w-3/4 py-10 text-slate-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+          <ContainerImage src={portrait13} alt="portrait" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+        </div>
+        <div className="w-3/4 py-10 text-slate-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+          <ContainerImage src={portrait16} alt="portrait" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>{" "}
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus
+            facere labore aperiam adipisci, illo voluptatibus. Quae numquam
+            nostrum consequuntur eum rem nemo!
+          </p>
+        </div>
+      </section>
       <div className="w-3/4 py-10 text-slate-500">
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
@@ -66,21 +140,7 @@ const loco = () => {
           aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
           eum rem nemo!
         </p>
-      </div>
-      <div className="w-3/4 py-10 text-slate-500">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus facere labore
-          aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
-          eum rem nemo!
-        </p>
-        <ContainerImage src={portrait} alt="portrait" />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          deleniti dolore nemo aliquid quam! Amet, fugiat? Ducimus facere labore
-          aperiam adipisci, illo voluptatibus. Quae numquam nostrum consequuntur
-          eum rem nemo!
-        </p>
+        <ContainerImage src={portrait3} alt="portrait" />
       </div>
     </>
   );
